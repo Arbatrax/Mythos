86ee45db8a9cd8e6b46b9b91fda81a6240cd6832
trippedoutfish <trippedoutfish@gmail.com>
2015-11-17 14:04:32 -0800

    Added altar buffs for wanderer.

--- game/customhero/scripts/vscripts/heroes/centaur_wizard/reckless_initiation.lua
+++ game/customhero/scripts/vscripts/heroes/centaur_wizard/reckless_initiation.lua
@@ -4,7 +4,7 @@ function Teleport(keys)
 	local radius = ability:GetLevelSpecialValueFor( "tp_range", ability:GetLevel() - 1 )
 	local target = keys.target_points[1]
 	local casterLocation = caster:GetAbsOrigin()
-	local nearbyAllies = FindUnitsInRadius(caster:GetTeam(), casterLocation, nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_ALL, 0, 0, false)
+	local nearbyAllies = FindUnitsInRadius(caster:GetTeam(), casterLocation, nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, 0, 0, false)
 	local count = table.getn(nearbyAllies)
 
 	for x=1, count do
--- game/customhero/scripts/vscripts/heroes/centaur_wizard/stone_shield.lua
+++ game/customhero/scripts/vscripts/heroes/centaur_wizard/stone_shield.lua
@@ -12,10 +12,11 @@ end
 
 function Absorb(keys)
 	local caster = keys.caster
+	local target = keys.target
 	local ability = keys.ability
 	local absorb = caster.shieldHealth
 	local damage = keys.attack_damage
-	if absorb - damage > 0 then
+	if absorb - damage > 0 and target.GetHealth() then
 		caster.shieldHealth = absorb - damage
 	else
 		caster:RemoveModifierByName("stone_shield")
@@ -31,7 +32,7 @@ function Explode(keys)
 	-- for x,y in pairs(keys) do
 	-- 	print(x,"|",y)
 	-- end
-	local nearbyUnits = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, 0, 0, false)
+	local nearbyUnits = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO+DOTA_UNIT_TARGET_CREEP, 0, 0, false)
 	local count = table.getn(nearbyUnits)
 
 	for x=1, count do
